716ccc60edb5f06fea910978e1f0247a7d79c990
Seongju Kang <ksj_design@naver.com>
2024-02-18 20:34:45 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,22 @@
+// 등차수열이란 첫번째 항부터 하나씩 더해지는 수열
+// 공차란 등차수열의 변화 값
+
+function solution(a, d, included) {
+
+    // 초기화
+    var answer = 0;
+
+    // 등차수열의 합(초기에는 a 대입)
+    var sum = a;
+
+    included.forEach((bln, idx) => {
+
+        // included[idx]가 참이면 공차 더함
+        answer += bln ? sum : 0;
+
+        // 등차수열 만들기
+        sum += d;
+    });
+
+    return answer;
+}
