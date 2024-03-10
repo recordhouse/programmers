2a87da93a27887fe65f523daee637f60d59167c9
Seongju Kang <ksj_design@naver.com>
2024-03-10 19:47:49 +0900

    [level 0] Title: 접미사 배열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,9 @@
+// 풀이 필요
+
+function solution(my_string) {
+    const strLen = my_string.length
+    return [...my_string].reduce((acc, cur, idx) => {
+        const curStr = my_string.slice(idx, strLen)
+        return [...acc, curStr]
+    }, []).sort()
+}
